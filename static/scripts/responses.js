8233c69c1fd0535f1ab807fac7bef3667a1578c3
JHONYUUI <115664640+JHONYUUI@users.noreply.github.com>
2022-11-29 16:11:06 +0000

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,59 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
+
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "Hello") {
+        return "Hello there!";
+    } else if (input == "Hello!") {
+        return "Hello my name is Chat Bot nice to meet you!";
+    } else if (input == "Hello.") {
+        return "Hi there!";
+    }
+
+    // Simple responses
+    if (input == "Hi") {
+        return "Hi nice to meet you!";
+    } else if (input == "Hi!") {
+        return "Hi!";
+    } else {
+        return "Try asking something else!";
+    }
+}
+
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "hi") {
+        return "Hello there!";
+    } else if (input == "Goodbye") {
+        return "Talk to you later!";
+    } else if (input == "Bye") {
+        return "Cya!";
+    }
+
+    // Simple responses
+    if (input == "bye") {
+        return "See ya later!";
+    } else if (input == "goodbye") {
+        return "Bye! I hope you had a nice time speaking to me!";
+    } else {
+        return "I do not understand try asking something else!";
+    }
+}
