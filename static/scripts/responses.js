40645ab9ae34145a75dd473974921760d10619cc
JHONYUUI <115664640+JHONYUUI@users.noreply.github.com>
2022-11-29 17:06:47 +0000

    Add files via upload

---
+++ static/scripts/responses.js
@@ -0,0 +1,99 @@
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } else if (input == "paper") {
+        return "scissors";
+    } else if (input == "scissors") {
+        return "rock";
+    }
+
+    // Simple responses
+    if (input == "hello") {
+        return "Hello there!";
+    } else if (input == "goodbye") {
+        return "Talk to you later!";
+    } else {
+        return "Try asking something else!";
+    }
+}
+
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "Hello") {
+        return "Hello there!";
+    } else if (input == "Hello!") {
+        return "Hello my name is Chat Bot nice to meet you!";
+    } else if (input == "Hello.") {
+        return "Hi there!";
+    }
+
+    // Simple responses
+    if (input == "Hi") {
+        return "Hi nice to meet you!";
+    } else if (input == "Hi!") {
+        return "Hi!";
+    } else {
+        return "Try asking something else!";
+    }
+}
+
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "hi") {
+        return "Hello there!";
+    } else if (input == "Goodbye") {
+        return "Talk to you later!";
+    } else if (input == "Bye") {
+        return "Cya!";
+    }
+
+    // Simple responses
+    if (input == "bye") {
+        return "See ya later!";
+    } else if (input == "goodbye") {
+        return "Bye! I hope you had a nice time speaking to me!";
+    } else {
+        return "I do not understand try asking something else!";
+    }
+}
+
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "good") {
+        return "That's brilliant to hear!";
+    } else if (input == "Good.") {
+        return "I'm happy that your happy!";
+    } else if (input == "Good") {
+        return "That's nice to hear!";
+    }
+
+    // Simple responses
+    if (input == "Brilliant!") {
+        return "Thats good to hear!";
+    } else if (input == "Brilliant.") {
+        return "That's the best thing I've heard all day!";
+    } else {
+        return "I do not understand try asking something else!";
+    }
+}
+
+function getBotResponse(input) {
+    //rock paper scissors
+    if (input == "Brilliant") {
+        return "That's brilliant to hear!";
+    } else if (input == "brilliant") {
+        return "I'm happy that your happy!";
+    } else if (input == "Brilliant?") {
+        return "That's nice to hear!";
+    }
+
+    // Simple responses
+    if (input == "Ping") {
+        return "Pong!";
+    } else if (input == "kldhgipergherq") {
+        return "qrehgrepihhdlk";
+    } else {
+        return "I do not understand try asking something else!";
+    }
+}
